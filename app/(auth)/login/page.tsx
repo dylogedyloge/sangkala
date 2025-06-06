cb108cdb53fafa86826d27eb4b76631112e535ff
dyloge <dylogedyloge@gmail.com>
2025-06-06 14:47:58 +0330

    third commit

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -16,7 +16,7 @@ export default function LoginPage() {
       >
         <LoginForm />
         <p className="text-center text-sm">
-          Don't have an account?{" "}
+          Don&apos;t have an account?{" "}
           <Link href="/signup" className="text-primary hover:underline">
             Sign up
           </Link>
